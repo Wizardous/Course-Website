1b068339cfbebc309b7d7d10864b5c600c6e5b33
Wizardous <abhijitgadge5599@gmail.com>
2021-12-08 00:08:51 +0530

    Updated AppController Class

--- Learning/src/main/java/com/learning/web/Controller/AppController.java
+++ Learning/src/main/java/com/learning/web/Controller/AppController.java
@@ -5,18 +5,42 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.learning.web.Entity.Course;
+import com.learning.web.Entity.User;
+import com.learning.web.Repository.CourseRepository;
+import com.learning.web.Service.CourseService;
+import com.learning.web.Service.UserService;
 
 @Controller
 public class AppController {
-
-	@RequestMapping("/")
+	
+	@Autowired
+	private CourseService courseService;
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/")
 	public String viewHomePage() {
 		return "index";
 	}
+	
+	@GetMapping("/dashboard/admin")
+	public String viewAdminDashboardPage() {
+		return "admin_dashboard";
+	}
+	
+	@GetMapping("/dashboard/user")
+	public String viewUserDahsboardPage() {
+		return "user_dashboard";
+	}
+
+	
+	
 
 }
