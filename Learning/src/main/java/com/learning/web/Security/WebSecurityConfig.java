a108aecccb556daa23e38ffceaf7da9a55def63d
Wizardous <abhijitgadge5599@gmail.com>
2021-12-08 12:35:35 +0530

    Added methods

--- Learning/src/main/java/com/learning/web/Security/WebSecurityConfig.java
+++ Learning/src/main/java/com/learning/web/Security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.learning.web.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,34 +21,36 @@ import com.learning.web.Security.UserDetailsServiceImpl;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	AuthenticationSuccessHandler successHandler;
-	
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.parentAuthenticationManager(authenticationManagerBean())
+			.userDetailsService(userDetailsService);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/user/**").hasAuthority("USER")
-			.antMatchers("/admin/**").hasAuthority("ADMIN")
-			.antMatchers("/signup").permitAll()
-			.antMatchers("/").permitAll()
-			.and().formLogin().successHandler(successHandler)
-			.and().logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/");
+		http.authorizeRequests().antMatchers("/user/**").hasAuthority("USER").antMatchers("/admin/**")
+				.hasAuthority("ADMIN").antMatchers("/signup").permitAll().antMatchers("/").permitAll().and().formLogin()
+				.successHandler(successHandler).and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
 //		.antMatchers("/courses").hasAnyAuthority("USER", "ADMIN")
-		
+
 		http.csrf().disable();
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
